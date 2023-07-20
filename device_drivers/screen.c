7bd3c24e151dce5ad64e22f40c416dc66a642669
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-19 22:08:01 -0400

    added in-progress device driver code

---
+++ device_drivers/screen.c
@@ -0,0 +1,18 @@
+#include "port_handling.h"
+
+void set_cursor(int offset) {
+    offset /= 2;
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset >> 8));
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset & 0xff));
+}
+
+int get_cursor() {
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+    int offset = port_byte_in(VGA_DATA_REGISTER) << 8;
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+    offset += port_byte_in(VGA_DATA_REGISTER);
+    return offset * 2;
+}
+

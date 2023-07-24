95e7e40160152bf6262758e319aafb1e1c077ed3
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-23 20:57:03 -0400

    added comments

--- bootloader/kernel_entry.asm
+++ bootloader/kernel_entry.asm
@@ -1,4 +1,7 @@
+global _start;
 [bits 32]
-[extern main] ; Define calling point. Must have same name as kernel.c 'main' function
-call main ; Calls the C function. The linker will know where it is placed in memory
-jmp $
+
+_start:
+    [extern start_kernel] ; Define calling point
+    call start_kernel ; Calls start_kernel function from kernel.c
+    jmp $

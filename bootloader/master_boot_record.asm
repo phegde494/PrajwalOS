fe1084e10f1ea9ebfe9da495df827fa2660fe977
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-23 20:58:17 -0400

    Update master_boot_record.asm

--- bootloader/master_boot_record.asm
+++ bootloader/master_boot_record.asm
@@ -1,35 +1,48 @@
-[bits 16]
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000
 
-KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
-
-mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl' on boot
+mov [BOOT_DRIVE], dl
 mov bp, 0x9000
 mov sp, bp
 
-call load_kernel ; read the kernel from disk
-call switch_to_32bit ; disable interrupts, load GDT,  etc. Finally jumps to 'BEGIN_PM'
-jmp $ ; Never executed
+mov bx, MSG_16BIT_MODE
+call print16
+call print16_nl
 
+call load_kernel ; read kernel from disk
+call switch_to_32bit ; disable interrupts, load GDT
+jmp $ 
+
+%include "bootloader/print_16_bit.asm"
+%include "bootloader/print_32_bit.asm"
 %include "bootloader/disk.asm"
 %include "bootloader/gdt.asm"
 %include "bootloader/switch_to_32_bit.asm"
 
 [bits 16]
 load_kernel:
-    mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
-    mov dh, 2
+    mov bx, MSG_LOAD_KERNEL
+    call print16
+    call print16_nl
+
+    mov bx, KERNEL_OFFSET ; read from disk and store in 0x1000
+    mov dh, 31
     mov dl, [BOOT_DRIVE]
     call disk_load
     ret
 
 [bits 32]
 BEGIN_32BIT:
-    call KERNEL_OFFSET ; Give control to the kernel
-    jmp $ ; Stay here when the kernel returns control to us (if ever)
+    mov ebx, MSG_32BIT_MODE
+    call print32
+    call KERNEL_OFFSET ; give control to kernel
+    jmp $ 
 
 
-BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
+BOOT_DRIVE db 0 
+MSG_16BIT_MODE db "Started in 16-bit Real Mode", 0
+MSG_32BIT_MODE db "Landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0
 
 ; padding
 times 510 - ($-$$) db 0

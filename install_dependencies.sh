b2ef1803074e8024e9fa9e5cf8bc3382b4568683
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-23 21:31:45 -0400

    added bash script for installing dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+brew install qemu nasm
+brew install i386-elf-binutils
+brew install i386-elf-gcc
+brew install i386-elf-gdb
+export PATH="/usr/local/Cellar/x86_64-elf-binutils/<version>/bin/:/usr/local/Cellar/x86_64-elf-gcc/<version>/bin/:/usr/local/Cellar/i386-elf-gdb/<version>/bin:$PATH"

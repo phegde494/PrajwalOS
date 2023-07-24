16d25abf8062b7237daced45231beea3930f1215
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-23 20:56:48 -0400

    added comments

--- bootloader/gdt.asm
+++ bootloader/gdt.asm
@@ -1,10 +1,9 @@
-gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
-    ; the GDT starts with a null 8-byte
+gdt_start: 
+    ; starts with a null 8-byte
     dd 0x0 ; 4 byte
     dd 0x0 ; 4 byte
 
-; GDT for code segment. base = 0x00000000, length = 0xfffff
-; for flags, refer to os-dev.pdf document, page 36
+; GDT for code segment - base = 0x00000000, length = 0xfffff
 gdt_code:
     dw 0xffff    ; segment length, bits 0-15
     dw 0x0       ; segment base, bits 0-15
@@ -13,8 +12,7 @@ gdt_code:
     db 11001111b ; flags (4 bits) + segment length, bits 16-19
     db 0x0       ; segment base, bits 24-31
 
-; GDT for data segment. base and length identical to code segment
-; some flags changed, again, refer to os-dev.pdf
+; GDT for data segment
 gdt_data:
     dw 0xffff
     dw 0x0
@@ -30,6 +28,6 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
     dd gdt_start ; address (32 bit)
 
-; define some constants for later use
+; define for later use
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start

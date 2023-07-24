f27028507279e89a007543786d24214e3b49e505
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-23 21:03:22 -0400

    added keyboard

---
+++ device_drivers/keyboard.c
@@ -0,0 +1,54 @@
+#include <stdbool.h>
+#include "keyboard.h"
+#include "ports.h"
+#include "../hardware_interrupts/isr.h"
+#include "screen.h"
+#include "../kernel/util.h"
+#include "../kernel/kernel.h"
+
+#define BACKSPACE 0x0E
+#define ENTER 0x1C
+
+static char key_buffer[256];
+
+#define SC_MAX 57
+
+// Model of QWERTY key names
+const char *sc_name[] = {"ERROR", "Esc", "1", "2", "3", "4", "5", "6",
+                         "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E",
+                         "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl",
+                         "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`",
+                         "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".",
+                         "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
+// Model of QWERTY key characters
+const char sc_ascii[] = {'?', '?', '1', '2', '3', '4', '5', '6',
+                         '7', '8', '9', '0', '-', '=', '?', '?', 'q', 'w', 'e', 'r', 't', 'y',
+                         'u', 'i', 'o', 'p', '[', ']', '?', '?', 'a', 's', 'd', 'f', 'g',
+                         'h', 'j', 'k', 'l', ';', '\'', '`', '?', '\\', 'z', 'x', 'c', 'v',
+                         'b', 'n', 'm', ',', '.', '/', '?', '?', '?', ' '};
+
+// Handles a key press
+static void keyboard_callback(registers_t *regs) {
+    uint8_t scancode = port_byte_in(0x60);
+    if (scancode > SC_MAX) return;
+    if (scancode == BACKSPACE) {
+        if (backspace(key_buffer)) {
+            print_backspace();
+        }
+    } else if (scancode == ENTER) {
+        print_nl();
+        execute_command(key_buffer);
+        key_buffer[0] = '\0';
+    } else {
+        char letter = sc_ascii[(int) scancode];
+        append(key_buffer, letter);
+        char str[2] = {letter, '\0'};
+        print_string(str);
+    }
+}
+
+// Initializes keyboard
+void init_keyboard() {
+    register_interrupt_handler(IRQ1, keyboard_callback);
+}
+

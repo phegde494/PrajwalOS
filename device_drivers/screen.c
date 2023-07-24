7ec267c8fd576dd3bf2f373a1e341995834bd368
phegde494 <48624928+phegde494@users.noreply.github.com>
2023-07-23 21:05:49 -0400

    screen functionality works

--- device_drivers/screen.c
+++ device_drivers/screen.c
@@ -1,18 +1,104 @@
+#include "screen.h"
 #include "port_handling.h"
+#include <stdint.h>
+#include "../kernel/mem.h"
+#include "../kernel/util.h"
 
+// Positions cursor in correct location
 void set_cursor(int offset) {
     offset /= 2;
-    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
-    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset >> 8));
-    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
-    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset & 0xff));
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char) (offset >> 8));
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char) (offset & 0xff));
 }
 
+// Gets current cursor location
 int get_cursor() {
-    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
-    int offset = port_byte_in(VGA_DATA_REGISTER) << 8;
-    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
-    offset += port_byte_in(VGA_DATA_REGISTER);
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    int offset = port_byte_in(REG_SCREEN_DATA) << 8; 
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    offset += port_byte_in(REG_SCREEN_DATA);
     return offset * 2;
 }
 
+// Gets offset
+int get_offset(int col, int row) {
+    return 2 * (row * MAX_COLS + col);
+}
+
+// Gets the row using the offset value
+int get_row_from_offset(int offset) {
+    return offset / (2 * MAX_COLS);
+}
+
+// Moves the offset to a new line and returns new offset
+int move_offset_to_new_line(int offset) {
+    return get_offset(0, get_row_from_offset(offset) + 1);
+}
+
+// Prints a character on the screen
+void set_char_at_video_memory(char character, int offset) {
+    uint8_t *vidmem = (uint8_t *) VIDEO_ADDRESS;
+    vidmem[offset] = character;
+    vidmem[offset + 1] = WHITE_ON_BLACK;
+}
+
+// Scrolls by copying disappearing content to memory and freeing space at bottom
+int scroll_ln(int offset) {
+    memory_copy(
+            (uint8_t * )(get_offset(0, 1) + VIDEO_ADDRESS),
+            (uint8_t * )(get_offset(0, 0) + VIDEO_ADDRESS),
+            MAX_COLS * (MAX_ROWS - 1) * 2
+    );
+
+    for (int col = 0; col < MAX_COLS; col++) {
+        set_char_at_video_memory(' ', get_offset(col, MAX_ROWS - 1));
+    }
+
+    return offset - 2 * MAX_COLS;
+}
+
+// Prints the given string in the shell
+void print_string(char *string) {
+    int offset = get_cursor();
+    int i = 0;
+    while (string[i] != 0) {
+        if (offset >= MAX_ROWS * MAX_COLS * 2) {
+            offset = scroll_ln(offset);
+        }
+        if (string[i] == '\n') {
+            offset = move_offset_to_new_line(offset);
+        } else {
+            set_char_at_video_memory(string[i], offset);
+            offset += 2;
+        }
+        i++;
+    }
+    set_cursor(offset);
+}
+
+// Prints a new line
+void print_nl() {
+    int newOffset = move_offset_to_new_line(get_cursor());
+    if (newOffset >= MAX_ROWS * MAX_COLS * 2) {
+        newOffset = scroll_ln(newOffset);
+    }
+    set_cursor(newOffset);
+}
+
+// Clears the shell
+void clear_screen() {
+    int screen_size = MAX_COLS * MAX_ROWS;
+    for (int i = 0; i < screen_size; ++i) {
+        set_char_at_video_memory(' ', i * 2);
+    }
+    set_cursor(get_offset(0, 0));
+}
+
+// Removes the last character, as the backspace button should
+void print_backspace() {
+    int newCursor = get_cursor() - 2;
+    set_char_at_video_memory(' ', newCursor);
+    set_cursor(newCursor);
+}
